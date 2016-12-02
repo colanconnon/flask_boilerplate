d9218335ea8c811f509742e632701cbcbf82d0a3
colan <cconnon11@gmail.com>
2016-12-01 21:58:39 -0500

    added bootstrap and finished login

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask.ext.sqlalchemy import SQLAlchemy
 import os
 from dotenv import load_dotenv, find_dotenv
 from flask import Flask, render_template
-from routes import routes
 
 load_dotenv(find_dotenv())
 
@@ -13,6 +12,8 @@ app = Flask(__name__)
 app.config.from_object(os.environ.get('APP_SETTINGS'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+# db needs to be setup before routes
+from routes import routes
 
 app.register_blueprint(routes)
 
--- routes.py
+++ routes.py
@@ -1,6 +1,6 @@
-from flask import Blueprint, render_template, redirect, url_for
-from models import User
+from flask import Blueprint, render_template, redirect, url_for, request
 from app import db
+from models import User
 
 
 routes = Blueprint('routes', __name__,
@@ -30,11 +30,19 @@ def register():
 @routes.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.filter_by(username=request.form['username']).first()
-        if user.check_password(request.form['password']):
-            flash('you are now signed in', 'success')
-            return redirect(url_for('index'))
-        else:
-            return render_template('login.html')
+        try:
+            if 'username' not in request.form or 'password' not in request.form:
+                return render_template('login.html', message={'error': "Username and password are required"})
+            user = User.query.filter_by(username=request.form['username']).first()
+            if user is None:
+                return render_template('login.html', message={'error' : "Error"})
+            if user.check_password(request.form['password']):
+                flash('you are now signed in', 'success')
+                return redirect(url_for('index'))
+            else:
+                return render_template('login.html', message={'error' : "Error"})
+        except Exception as e:
+            print(e)
+            return render_template('login.html', message={'error' : "Error"})
     else:
         return render_template('login.html')
